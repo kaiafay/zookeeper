57fea4066ade62bafa375561df2dd05716e8f192
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-01 17:09:45 -0700

    add listen method and get method to return json to client

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+const { animals } = require('./data/animals.json');
 const express = require('express');
 // assign express() to a variable so we can chain methods to the server
 const app = express();
+
+// the get() method requires two arguments: 
+// the route the client will fetch from and a callback function that executes when that route is accessed with a GET request
+app.get('/api/animals', (req, res) => {
+    // sends json to the client
+    res.json(animals);
+});
 
 // tells server to listen for requests
 // port numbers around 3000 are common practice

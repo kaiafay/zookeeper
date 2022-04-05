ef570f86881fab59a76a400fba1fc09b3023472c
Kaia Scheirman <kfscheirman18@gmail.com>
2022-04-05 12:26:20 -0700

    add form handling for zookeeper

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -2,11 +2,12 @@
 
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeeperRoutes = require('../apiRoutes/zookeeperRoutes');
 
 // have the router use the module exported from animalRoutes.js
 router.use(animalRoutes);
 // have the router use the module exported from zookeeperRoutes.js
-router.use(require('./zookeeperRoutes'));
+router.use(zookeeperRoutes);
 
 // export the router
 module.exports = router;
